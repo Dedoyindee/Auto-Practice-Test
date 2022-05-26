b8552ffada2a136c0b04d8c7eceec4ff54c3d902
clemagbaje <clemagbaje@gmail.com>
2022-05-26 13:21:47 +0100

    Add project files.

---
+++ StepDefinitions/FirstNameStartWithLMNOPSteps.cs
@@ -0,0 +1,50 @@
+﻿using DigitalAutomationQuoteTest.AutomationFramework.PageObjects;
+using DigitalAutomationQuoteTest.Extensions;
+using DigitalAutomationQuoteTest.Helpers;
+using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+
+namespace DigitalAutomationQuoteTest.StepDefinitions
+{
+    [Binding]
+    public sealed class FirstNameStartWithLMNOPSteps
+    {
+        private readonly ScenarioContext scenarioContext;
+
+        public FirstNameStartWithLMNOPSteps(ScenarioContext scenarioContext)
+        {
+            this.scenarioContext = scenarioContext;
+        }
+
+        private AutomationTechTestLandingPage AutomationTechTestLandingPage { get { return new AutomationTechTestLandingPage(this.scenarioContext.Driver()); } }
+
+        [When(@"I '(.*)' click on < button  option")]
+        public void WhenIOnButtonOption(string fname)
+        {
+            if (fname.ToUpper().StartsWith("L") || fname.ToUpper().StartsWith("M") || fname.ToUpper().StartsWith("N") || fname.ToUpper().StartsWith("O") || fname.ToUpper().StartsWith("P"))
+            {
+                Wait.Until(TimeSpan.FromSeconds(10), () => this.AutomationTechTestLandingPage.SignButton.Displayed);
+                this.AutomationTechTestLandingPage.SignButton.Click();
+            }
+            else
+            {
+                Console.WriteLine("FIRST NAME DOES NOT Start With Letter L_M_N_O_P");
+                Assert.True(1 == 2);
+            }
+
+        }
+
+
+        [Then(@"option panel is hidden")]
+        public void ThenOptionPanelIsHidden()
+        {
+            Wait.Until(TimeSpan.FromSeconds(15), () => this.AutomationTechTestLandingPage.OptionsLabel.Displayed);
+            var optiondisplay = this.AutomationTechTestLandingPage.OptionsLabel.Displayed;
+            Assert.IsFalse(optiondisplay);
+        }
+
+
+
+    }
+}
---
+++ StepDefinitions/HelloWorldSteps.cs
@@ -0,0 +1,44 @@
+﻿using DigitalAutomationQuoteTest.AutomationFramework.PageObjects;
+using DigitalAutomationQuoteTest.Extensions;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace DigitalAutomationQuoteTest.StepDefinitions
+{
+    [Binding]
+    public sealed class HelloWorldSteps
+    {
+        private readonly ScenarioContext scenarioContext;
+
+        public HelloWorldSteps(ScenarioContext scenarioContext)
+        {
+            this.scenarioContext = scenarioContext;
+        }
+
+        private AutomationTechTestLandingPage AutomationTechTestLandingPage { get { return new AutomationTechTestLandingPage(this.scenarioContext.Driver()); } }
+
+        [Given(@"the user is on the dotnetfiddle\.net landing page")]
+        public void GivenTheUserIsOnTheDotnetfiddle_NetLandingPage()
+        {
+            
+        }
+
+        [When(@"I click run button")]
+        public void WhenIClickRunButton()
+        {
+            this.AutomationTechTestLandingPage.RunButton.Click();
+        }
+
+
+        [Then(@"I should see '(.*)' in output window")]
+        public void ThenIShouldSeeInOutputWindow(string txtdisplay)
+        {
+            Assert.AreEqual(txtdisplay, this.AutomationTechTestLandingPage.TextMessage);
+        }
+
+
+
+
+
+    }
+}
